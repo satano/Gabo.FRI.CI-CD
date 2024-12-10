65b19c387607118b6b33cda26b514f3d8fcb54c0
Stano Gabo Peťko <stano@stanopetko.eu>
2024-12-10 13:09:40 +0100

    Add tests

---
+++ MMLib.Fri.MinimalAPI.Tests/CreateContactValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using FluentValidation.Results;
+using static MMLib.Fri.MinimalAPI.Features.Contacts.CreateContactRequest;
+
+namespace MMLib.Fri.MinimalAPI.Tests
+{
+    public class CreateContactValidatorTests
+    {
+        [Fact]
+        public void Valid()
+        {
+            CreateContactValidator validator = new();
+
+            CreateContact request = new()
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "johndoe@example.com"
+            };
+
+            ValidationResult result = validator.Validate(request);
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Invalid()
+        {
+            CreateContactValidator validator = new();
+
+            CreateContact request = new()
+            {
+                FirstName = "",
+                LastName = "",
+                Email = ""
+            };
+
+            ValidationResult result = validator.Validate(request);
+            result.IsValid.Should().BeFalse();
+        }
+    }
+}
